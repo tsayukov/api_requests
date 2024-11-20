e0a87962dea39756816d9f06592d6ad6295968cf
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-11-20 16:12:20 +0300

    Add paginator interface

---
+++ api_requests/details/_paginator.py
@@ -0,0 +1,11 @@
+"""Interface of a paginator."""
+
+from collections.abc import Callable, Iterable
+from dataclasses import dataclass
+from typing import Concatenate
+
+
+@dataclass(slots=True, frozen=True, eq=False, match_args=False, kw_only=True)
+class PaginatorInterface[Cls: object, R, **P]:
+    paginator: Callable[Concatenate[Cls, P], R]
+    api_methods: Iterable[str]
