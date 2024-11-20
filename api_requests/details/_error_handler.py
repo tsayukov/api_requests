54910c2bb30e7df77f21923260c5c1fd8e6aa268
Pavel Tsayukov <p.tsayukov@gmail.com>
2024-11-20 16:11:57 +0300

    Add error handler interface

---
+++ api_requests/details/_error_handler.py
@@ -0,0 +1,12 @@
+"""Interface for an error handler."""
+
+from collections.abc import Callable, Iterable
+from dataclasses import dataclass
+from typing import Concatenate
+
+
+@dataclass(slots=True, frozen=True, eq=False, match_args=False, kw_only=True)
+class ErrorHandlerInterface[Cls: object, R, **P]:
+    error_handler: Callable[Concatenate[Cls, P], R]
+    api_methods: Iterable[str]
+    error_codes: int | Iterable[int] | None
